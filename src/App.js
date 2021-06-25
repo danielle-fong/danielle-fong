0c242bb124dfb27f8369111e99f067c32583a9cc
danielle-fong <daniellefong13@gmail.com>
2021-06-25 12:13:00 -0400

    commit on main

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Homepage from "./website/pages/Homepage";
 import Projects from "./website/pages/Projects";
 import Other from "./website/pages/Other";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./website/Nav";
 
 const App = () => {
 	return (
 		<>
 			<Router>
-				<Navigation />
+				<Nav />
 				<Switch>
 					<Route path="/" exact component={Homepage} />
 					<Route path="/how-it-works" exact component={Projects} />
