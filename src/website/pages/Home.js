5295d69b997d410ae77af527291a61d0ead3f66a
danielle-fong <daniellefong13@gmail.com>
2021-06-25 13:47:34 -0400

    commit on page

--- src/website/pages/Home.js
+++ src/website/pages/Home.js
@@ -3,7 +3,16 @@ import "./Home.css";
 function Home(){
     return (
         <>
-            <h1 className = "header">Danielle Fong</h1>
+            <div className = "intro">
+                <div className = "intro-text">
+                    <h1 className = "header">ELECTRICAL ENGINEER</h1>
+                    <h2 className = "name">Danielle Fong</h2>
+                    <h3 className = "text">insert text here about yourself for intro page</h3>
+                </div>
+                <div className = "profile-pic">
+                    <h4 className = "image"> insert image here</h4>
+                </div>
+            </div>
         </>
     );
 }
