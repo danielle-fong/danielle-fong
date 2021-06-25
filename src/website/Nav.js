a0d9e99f36ed555ebcfe7406ea65113b85cd7935
danielle-fong <daniellefong13@gmail.com>
2021-06-25 13:34:49 -0400

    changes to home

--- src/website/Nav.js
+++ src/website/Nav.js
@@ -10,12 +10,12 @@ const Nav = () => {
 				</Link>
 				<div className="link-wrapper">
 					<Link className="nav-link" to="/">
-						<h2>Homepage</h2>
+						<h2>Home</h2>
 					</Link>
-					<Link className="nav-link" to="/about">
+					<Link className="nav-link" to="/Projects">
 						<h2>Projects</h2>
 					</Link>
-					<Link className="nav-link" to="/how-it-works">
+					<Link className="nav-link" to="/Other">
 						<h2>Other</h2>
 					</Link>
 				</div>
