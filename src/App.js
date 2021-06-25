6ae9622b06146a4033f1df20599d2954d069a3b2
danielle-fong <daniellefong13@gmail.com>
2021-06-25 11:28:42 -0400

    adding pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 import Homepage from "./website/Homepage";
+import Projects from "./website/Projects";
+import Other from "./website/Other";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const App = () => {
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Homepage} />
+					<Route path="/how-it-works" exact component={Projects} />
+					<Route path="/about" exact component={Other} />
+				</Switch>
+			</Router>
+		</>
+	);
+};
 
 export default App;
 
