6b6dc16c06937c2cab4b12c48777f0cf5364aa27
danielle-fong <daniellefong13@gmail.com>
2021-06-25 11:19:55 -0400

    commit on main

--- src/App.js
+++ src/App.js
@@ -1,16 +1,5 @@
 import "./App.css";
 import Homepage from "./website/Homepage";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-const App = () => {
-	return (
-		<>
-			<Router>
-					<Route path="/" exact component={Homepage} />
-			</Router>
-		</>
-	);
-};
 
 export default App;
 
