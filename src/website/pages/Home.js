68220738e670e916481722bf6598b703a6ef544a
danielle-fong <daniellefong13@gmail.com>
2021-06-26 23:59:30 -0400

    commit on home

--- src/website/pages/Home.js
+++ src/website/pages/Home.js
@@ -1,5 +1,9 @@
 import "./Home.css";
 import github from "./github.png"
+import linkedin from "./linkedin.png"
+import resume from "./resume.png"
+import email from "./email.png"
+import devpost from "./devpost.png"
 
 function Home(){
     return (
@@ -13,6 +17,10 @@ function Home(){
             </div>
             <div className = "social">
                 <img className="github" src={github}/>
+                <img className="linkedin" src={linkedin}/>
+                <img className="resume" src={resume}/>
+                <img className="email" src={email}/>
+                <img className="devpost" src={devpost}/>
             </div>
         </>
     );
