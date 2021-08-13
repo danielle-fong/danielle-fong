9bf4636d03c1e05aee779762218adf26cfabe8d2
danielle-fong <daniellefong13@gmail.com>
2021-08-13 19:23:54 -0400

    commit on projects

--- src/website/pages/Projects.js
+++ src/website/pages/Projects.js
@@ -1,9 +1,15 @@
 import "./Projects.css";
 import colourme from "./colourme.png";
-import recycle from "./recycle.png";
+import colourme_gif from "./colourme.gif";
+import recycler from "./recycler.png";
+import recycler_gif from "./recycler.gif";
 import marblemod from "./marblemod.png";
+import marblemod_gif from "./marblemod.gif";
 import vbook from "./vbook.png";
+import vbook_gif from "./vbook.gif";
 import cadmodel from "./cadmodel.png";
+import cadmodel_gif from "./cadmodel.gif";
+
 
 
 function Projects(){
@@ -14,45 +20,68 @@ function Projects(){
 
             <div className = "row1">
                 <div className = "p1">
-                    <h1 className = "t1">
-                        ColourME is a colour palette generator designed to create the perfect palette for the user. 
-                        This project was created using ReactJS, JavaScript, HTML, and CSS. 
-                    </h1>
                     <img className="colourme" src={colourme}/>
-                </div>
-                <div className = "p2">
-                    <h1 className = "t2">
-                        Recycler is a program that organizes recyclable containeers by using sensors to 
-                        determine the material and which bin it should be disposed in.
-                        This project was coded in Python.
-                    </h1>
-                    <img className="recycle" src={recycle}/>
-                </div>
+                    <div className="colourme_gif">
+						<img src={colourme_gif} width = "600px" height = "370px"/>
+				    </div>
+                </div> 
+                <h1 className = "t1">
+                    ColourME is a colour palette generator designed to create the perfect palette for the user. 
+                    This project was created using ReactJS, JavaScript, HTML, and CSS.
+                </h1>
             </div>
 
             <div className = "row2">
-                <div className = "p3">
-                    <h1 className = "t3">
-                        Marble Mod adds decorative marble blocks into your Minecraft world. 
-                        This mod was created using FabricMC's mod template and coded in Java. 
-                        </h1>
-                    <img className="marblemod" src={marblemod}/>
-                </div>
-                <div className = "p4">
-                    <h1 className = "t4">
-                        Vbook is a Facebook Messenger extension for speech to text chatting 
-                        that includes a bot to accept and give responses to the user. 
-                        This project was coded in Python and uses IBM Watson and Facebook Messenger API.
-                    </h1>
-                    <img className="vbook" src={vbook}/>
-                </div>
+                <div className = "p2">
+                    <img className="recycler" src={recycler}/>
+                    <div className="recycler_gif">
+						<img src={recycler_gif} width = "600px" height = "370px"/>
+				    </div>
+                </div> 
+                <h1 className = "t2">
+                    Recycler is a program that organizes recyclable containeers by using sensors to 
+                    determine the material and which bin it should be disposed in.
+                    This project was coded in Python.                
+                </h1>
             </div>
 
             <div className = "row3">
+                <div className = "p3">
+                    <img className="marblemod" src={marblemod}/>
+                    <div className="marblemod_gif">
+						<img src={marblemod_gif} width = "600px" height = "370px"/>
+				    </div>
+                </div> 
+                <h1 className = "t3">
+                    Marble Mod adds decorative marble blocks into your Minecraft world. 
+                    This mod was created using FabricMC's mod template and coded in Java.              
+                </h1>
+            </div>
+
+            <div className = "row4">
+                <div className = "p4">
+                    <img className="vbook" src={vbook}/>
+                    <div className="vbook_gif">
+						<img src={vbook_gif} width = "600px" height = "370px"/>
+				    </div>
+                </div> 
+                <h1 className = "t4">
+                    Marble Mod adds decorative marble blocks into your Minecraft world. 
+                    This mod was created using FabricMC's mod template and coded in Java.              
+                </h1>
+            </div>
+
+            <div className = "row5">
                 <div className = "p5">
-                    <h1 className = "t5">cadmodel is a project designed</h1>
                     <img className="cadmodel" src={cadmodel}/>
-                </div>
+                    <div className="cadmodel_gif">
+						<img src={cadmodel_gif} width = "600px" height = "370px"/>
+				    </div>
+                </div> 
+                <h1 className = "t5">
+                    cad adds decorative cad marble blocks into your Minecraft world. 
+                    This mod was created using FabricMC's mod template and coded in Java.
+                </h1>
             </div>
 
             </div>            
