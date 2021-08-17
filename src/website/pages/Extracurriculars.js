5f0b1e7b980db87485b0da87a59d35a26de37e92
danielle-fong <daniellefong13@gmail.com>
2021-08-17 12:43:29 -0400

    commit on pages

--- src/website/pages/Extracurriculars.js
+++ src/website/pages/Extracurriculars.js
@@ -9,11 +9,30 @@ function Extracurriculars(){
             </div>
 
             <div className = "extra">
-                <h1 className = e1>
-                    
-                </h1>
+                <div className = "b1">
+                    <h1 className = "e1">Frosh Leader, Hamilton Chinese Christian Fellowship</h1>
+                    <h1 className = "y1">2021</h1>
+                </div>
 
+                <div className = "b2">
+                    <h1 className = "e2">Public Relations Committee Member, IEEE McMaster Branch</h1>
+                    <h1 className = "y2">2020-2021</h1>
+                </div>
 
+                <div className = "b3">
+                    <h1 className = "e3">Executive Chairman, Markville True Vine Christian Fellowship</h1>
+                    <h1 className = "y3">2017-2020</h1>
+                </div>
+
+                <div className = "b4">
+                    <h1 className = "e4">Music Team Lead Guitarist, AFC Teens Conference</h1>
+                    <h1 className = "y4">2017-2020</h1>
+                </div>
+
+                <div className = "b5">
+                    <h1 className = "e5">Assistant Softball Coach, Chinese Christian Softball Association</h1>
+                    <h1 className = "y5">2019</h1>
+                </div>
             </div>
         </>
     )
