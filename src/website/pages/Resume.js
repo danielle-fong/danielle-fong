ff34ffc7b9bd06e48e5516ef4cadb66b85148695
danielle-fong <daniellefong13@gmail.com>
2021-08-31 16:14:45 -0400

    commit on resume

--- src/website/pages/Resume.js
+++ src/website/pages/Resume.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
-
+import { Document, Page, pdfjs } from "react-pdf";
+import { PDFViewer } from '@react-pdf/renderer';
+import pdf from "./DanielleFongResume.pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 function Resume(){
     return (
-        <div>
-            <Document file="DanielleFongResume.pdf" />
-        </div>
+        <PDFViewer width="1100" height="900" className="pdf" >
+            <Document file={pdf} />
+        </PDFViewer>    
     )
 }
-
 export default Resume;
